449d02795f75e274e1ff86ac23d95aa9e416f416
Kangz <corentin@wallez.net>
2012-01-03 00:12:42 +0100

    Templated class' members should be in the .h ...

--- src/core/blocking-queue.h
+++ src/core/blocking-queue.h
@@ -25,13 +25,63 @@ namespace Epyx {
         void push(T& e);
         bool tryPush(T& e);
         T* pop();
-        T* pop(int msec);        
+        T* pop(int msec);
         T* tryPop();
 
         //Do not implement the following methods due to a lack of atomic operations
         //int length();
         //bool isEmpty();
     };
+
+    template<typename T> BlockingQueue<T>::BlockingQueue() {}
+    template<typename T> BlockingQueue<T>::~BlockingQueue() {} //Who frees the elements ?
+
+    template<typename T> void BlockingQueue<T>::push(T& e) {
+        cond.lock();
+        fifo.push(e);
+        cond.notify();
+        cond.unlock();
+    }
+
+    template<typename T> bool BlockingQueue<T>::tryPush(T& e) {
+        if(cond.tryLock()){
+            fifo.push(e);
+            cond.notify();
+            cond.unlock();
+            return true;
+        }
+        return false;
+    }
+
+    template<typename T> T* BlockingQueue<T>::pop(){
+        cond.lock();
+        while(fifo.empty()){
+            cond.wait();
+        }
+        T& result = fifo.front();
+        fifo.pop();
+        return &result;
+    }
+
+    template<typename T> T* BlockingQueue<T>::pop(int msec){
+        cond.lock();
+        while(fifo.empty()){
+            cond.timedWait(msec);
+        }
+        T& result = fifo.front();
+        fifo.pop();
+        return &result;
+    }
+
+    template<typename T> T* BlockingQueue<T>::tryPop(){
+        if(cond.tryLock()){
+            T& result = fifo.front();
+            fifo.pop();
+            cond.unlock();
+            return &result;
+        }
+        return NULL;
+    }
 }
 
 #endif /* EPYX_BLOCKING_QUEUE_H */
