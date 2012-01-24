42343be175fa090946d3a6c297202c0cb565883f
Corentin Wallez <corentin@wallez.net>
2012-01-24 17:21:18 +0100

    Thread Local Storage is here (TLSPointer)

---
+++ src/core/tls-pointer.h
@@ -0,0 +1,81 @@
+/**
+ */
+#ifndef EPYX_TLS_POINTER_H
+#define EPYX_TLS_POINTER_H
+
+#include <pthread.h>
+#include "exception.h"
+
+namespace Epyx {
+
+    template<typename T> class TLSPointer {
+    private:
+        // Disable copy construction and assignment.
+        TLSPointer(const TLSPointer&);
+        const TLSPointer& operator=(const TLSPointer&);
+
+        void (*destructor)(T*);
+        T* (*constructor)();
+        pthread_key_t* key;
+
+        typedef void(void_destructor_func)(void*);
+    public:
+        static void _default_destructor(T* object);
+
+        TLSPointer(void (*destructor)(T*) = &_default_destructor);
+        TLSPointer(T* (*constructor)(), void (*destructor)(T*) = &_default_destructor);
+        ~TLSPointer();
+
+        T* get();
+        void reset(T* new_value=NULL);
+        T* release();
+    };
+
+    template<typename T> void TLSPointer<T>::_default_destructor(T* object){
+        delete object;
+    }
+
+    template<typename T> TLSPointer<T>::TLSPointer(void (*destructor_)(T*))
+        :destructor(destructor_), constructor(NULL), key(new pthread_key_t) {
+        if( pthread_key_create(this->key, reinterpret_cast<void_destructor_func*>(this->destructor)) ){
+            throw FailException("TLSPointer", "pthread_key_create error");
+        }
+    }
+
+    template<typename T> TLSPointer<T>::TLSPointer(T* (*constructor_)(), void (*destructor_)(T*))
+        :destructor(destructor_), constructor(constructor_), key(new pthread_key_t){
+        if( pthread_key_create(this->key, reinterpret_cast<void_destructor_func*>(this->destructor)) ){
+            throw FailException("TLSPointer", "pthread_key_create error");
+        }
+    }
+
+    template<typename T> TLSPointer<T>::~TLSPointer(){//TODO
+    };
+
+    template<typename T> T* TLSPointer<T>::get(){
+        T* result = static_cast<T*>(pthread_getspecific(*this->key));
+
+        if(result == NULL && this->constructor != NULL){
+            result = this->constructor();
+            pthread_setspecific(*this->key, static_cast<void *>(result));
+        }
+
+        return result;
+    }
+
+    template<typename T> void TLSPointer<T>::reset(T* new_value){
+        T* current = static_cast<T*>(pthread_getspecific(*this->key));
+
+        if(new_value != current){
+            pthread_setspecific(*this->key, static_cast<void *>(new_value));
+        }
+    }
+
+    template<typename T> T* TLSPointer<T>::release(){
+        T* temp = static_cast<T*>(pthread_getspecific(*this->key));
+        pthread_setspecific(*this->key, NULL);
+        return temp;
+    }
+}
+
+#endif /* EPYX_TLS_POINTER_H */
