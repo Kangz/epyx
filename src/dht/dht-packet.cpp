f54a1a6fa7c9f16a49e123a02952062cad3b4b93
Corentin Wallez <corentin@wallez.net>
2012-04-15 23:28:50 +0200

    DHTPacket: fix a few bugs (eg lowercase) and make ground for the DHTNode

--- src/dht/dht-packet.cpp
+++ src/dht/dht-packet.cpp
@@ -14,10 +14,10 @@ namespace DHT
 
     DHTPacket::DHTPacket(GTTPacket& pkt){
         //TODO: what should we do if a packet is not valid ?
-        if(pkt.headers.count("From")==0){
+        if(pkt.headers.count("from")==0){
             throw;
         }
-        std::istringstream ss(pkt.headers["From"]);
+        std::istringstream ss(pkt.headers["from"]);
         ss >> from;
 
         if(pkt.method == "PING"){
@@ -28,71 +28,66 @@ namespace DHT
 
         }else if(pkt.method == "STORE"){
             method = M_STORE;
-            if(pkt.headers.count("ConnectionId") == 0 || pkt.headers.count("Key") == 0 || !pkt.body){
+            if(pkt.headers.count("connectionid") == 0 || pkt.headers.count("key") == 0 || !pkt.body){
                 throw;
             }
 
-            value = pkt.acquireBody();
-            valueSize = pkt.size;
-            connectionId = String::toInt(pkt.headers["ConnectionId"]);
+            value = std::string(pkt.acquireBody(), pkt.size);
+            connectionId = String::toInt(pkt.headers["connectionid"]);
 
-            std::stringstream sst(pkt.headers["Key"]);
-            sst >> key;
-
+            key = pkt.headers["key"];
 
         }else if(pkt.method == "STORED"){
             method = M_STORED;
-            if(pkt.headers.count("ConnectionId") == 0 || pkt.headers.count("Status") == 0){
+            if(pkt.headers.count("connectionid") == 0 || pkt.headers.count("status") == 0){
                 throw;
             }
 
-            connectionId = String::toInt(pkt.headers["ConnectionId"]);
-            status = String::toInt(pkt.headers["Status"]);
+            connectionId = String::toInt(pkt.headers["connectionid"]);
+            status = String::toInt(pkt.headers["status"]);
 
         }else if(pkt.method == "GET"){
             method = M_GET;
-            if(pkt.headers.count("ConnectionId") == 0 || pkt.headers.count("Key") == 0){
+            if(pkt.headers.count("connectionid") == 0 || pkt.headers.count("key") == 0){
                 throw;
             }
 
-            connectionId = String::toInt(pkt.headers["ConnectionId"]);
+            connectionId = String::toInt(pkt.headers["connectionid"]);
 
-            std::stringstream sst(pkt.headers["Key"]);
-            sst >> key;
+            key = pkt.headers["Key"];
 
 
         }else if(pkt.method == "GOT"){
             method = M_GOT;
-            if(pkt.headers.count("ConnectionId") == 0 || !pkt.body){
+            if(pkt.headers.count("connectionid") == 0 || pkt.headers.count("status") == 0 || !pkt.body){
                 throw;
             }
 
-            value = new char[pkt.size];
-            valueSize = pkt.size;
-            connectionId = String::toInt(pkt.headers["ConnectionId"]);
+            value = std::string(pkt.body, pkt.size);
+            connectionId = String::toInt(pkt.headers["connectionid"]);
 
         }else if(pkt.method == "FIND"){
             method = M_FIND;
-            if(pkt.headers.count("ConnectionId") == 0 || pkt.headers.count("Count") == 0 ||
-               pkt.headers.count("Target") == 0){
+            if(pkt.headers.count("connectionid") == 0 || pkt.headers.count("count") == 0 ||
+               pkt.headers.count("target") == 0){
                 throw;
             }
 
-            connectionId = String::toInt(pkt.headers["ConnectionId"]);
-            count = String::toInt(pkt.headers["Count"]);
+            connectionId = String::toInt(pkt.headers["connectionid"]);
+            count = String::toInt(pkt.headers["count"]);
 
-            std::stringstream sst(pkt.headers["Target"]);
+            std::stringstream sst(pkt.headers["target"]);
             sst >> idToFind;
 
         }else if(pkt.method == "FOUND"){
             method = M_FOUND;
-            if(pkt.headers.count("ConnectionId") == 0 || pkt.headers.count("Count") == 0 ||
-               pkt.headers.count("Status") == 0 || !pkt.body){
+            if(pkt.headers.count("connectionid") == 0 || pkt.headers.count("count") == 0 ||
+               pkt.headers.count("status") == 0 || !pkt.body){
                 throw;
             }
-            connectionId = String::toInt(pkt.headers["ConnectionId"]);
-            count = String::toInt(pkt.headers["Count"]);
-            status = String::toInt(pkt.headers["Status"]);
+            connectionId = String::toInt(pkt.headers["connectionid"]);
+            count = String::toInt(pkt.headers["count"]);
+            status = String::toInt(pkt.headers["status"]);
 
             if(pkt.size != count*49){ //HACK: this is the size of the hexadecimal representation of an Id
                 return;//throw;
@@ -117,7 +112,7 @@ namespace DHT
 
         std::ostringstream oss;
         oss << from;
-        pkt->headers["From"] = oss.str();
+        pkt->headers["from"] = oss.str();
         oss.str("");
 
         switch(method){
@@ -132,61 +127,55 @@ namespace DHT
             case M_STORE:
                 pkt->method = "STORE";
 
-                oss << key;
-                pkt->headers["Key"] = oss.str();
-                oss.str("");
+                pkt->headers["key"] = key;
 
-                pkt->headers["ConnectionId"] = String::fromInt(connectionId);
+                pkt->headers["connectionid"] = String::fromInt(connectionId);
 
-                pkt->body = value;
-                value = NULL; //To avoid to free it after we give away the GTTPacket
-                pkt->size = valueSize;
+                pkt->body = String::toNewChar(value);
+                pkt->size = value.size();
                 break;
 
             case M_STORED:
                 pkt->method = "STORED";
-                pkt->headers["ConnectionId"] = String::fromInt(connectionId);
-                pkt->headers["Status"] = String::fromInt(status);
+                pkt->headers["connectionid"] = String::fromInt(connectionId);
+                pkt->headers["status"] = String::fromInt(status);
                 break;
 
             case M_GET:
                 pkt->method = "GET";
 
-                oss << key;
-                pkt->headers["Key"] = oss.str();
-                oss.str("");
+                pkt->headers["key"] = key;
 
-                pkt->headers["ConnectionId"] = String::fromInt(connectionId);
+                pkt->headers["connectionid"] = String::fromInt(connectionId);
                 break;
 
             case M_GOT:
                 pkt->method = "GOT";
 
-                pkt->headers["ConnectionId"] = String::fromInt(connectionId);
-                pkt->headers["Status"] = String::fromInt(status);
+                pkt->headers["connectionid"] = String::fromInt(connectionId);
+                pkt->headers["status"] = String::fromInt(status);
 
-                pkt->body = value;
-                value = NULL; //To avoid to free it after we give away the GTTPacket
-                pkt->size = valueSize;
+                pkt->body = String::toNewChar(value);
+                pkt->size = value.size();
                 break;
 
             case M_FIND:
                 pkt->method = "FIND";
 
-                pkt->headers["ConnectionId"] = String::fromInt(connectionId);
-                pkt->headers["Count"] = String::fromInt(count);
+                pkt->headers["connectionid"] = String::fromInt(connectionId);
+                pkt->headers["count"] = String::fromInt(count);
 
                 oss << idToFind;
-                pkt->headers["Target"] = oss.str();
+                pkt->headers["target"] = oss.str();
                 oss.str("");
                 break;
 
             case M_FOUND:
                 pkt->method = "FOUND";
 
-                pkt->headers["ConnectionId"] = String::fromInt(connectionId);
-                pkt->headers["Status"] = String::fromInt(status);
-                pkt->headers["Count"] = String::fromInt(foundIds.size());
+                pkt->headers["connectionid"] = String::fromInt(connectionId);
+                pkt->headers["status"] = String::fromInt(status);
+                pkt->headers["count"] = String::fromInt(foundIds.size());
 
                 for(std::list<Id>::iterator i=foundIds.begin(); i != foundIds.end(); ++i){
                     oss << *i;
