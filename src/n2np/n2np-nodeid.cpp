8b3a84ab7275bfdde00eb976b57348c1dd153a4b
Nicolas Iooss <nicolas.iooss@polytechnique.org>
2012-04-18 20:37:37 +0200

    N2NP: Complete NodeId class. Small bugfix in IPv6 parsing in net/Address
    Signed-off-by: Nicolas Iooss <nicolas.iooss@polytechnique.org>

--- src/n2np/n2np-nodeid.cpp
+++ src/n2np/n2np-nodeid.cpp
@@ -1,49 +1,68 @@
 #include "n2np-nodeid.h"
-#include <string.h>
+#include "../core/common.h"
 
 namespace Epyx
 {
+
     N2npNodeId::N2npNodeId()
-        :relay()
-    {
+    :name(), relay() {
+    }
+
+    N2npNodeId::N2npNodeId(const Address& relay)
+    :name(), relay(relay) {
         this->name[0] = 0;
     }
 
-     N2npNodeId::N2npNodeId(const Address relay_)
-        :relay(relay_)
-    {
-        this->name[0] = 0;
+    N2npNodeId::N2npNodeId(const std::string& name, const Address& relay)
+    :name(name), relay(relay) {
     }
 
-   N2npNodeId::N2npNodeId(const char *name_, const Address relay_)
-        :relay(relay_)
-    {
-        strncpy(this->name, name_, N2npNodeNameMaxLen);
-        this->name[N2npNodeNameMaxLen] = 0;
+    N2npNodeId::N2npNodeId(const std::string& str) {
+        size_t pos = str.find('@');
+        if (pos == std::string::npos) {
+            log::error << "N2NP: Arobas is missing in " << str << log::endl;
+            throw FailException("N2np::NodeId", "No @ in ID");
+        }
+        name = str.substr(0, pos);
+        relay = Address(str.substr(pos+1));
     }
 
-    std::ostream& operator<<(std::ostream& os, const N2npNodeId nodeid)
-    {
+    N2npNodeId::N2npNodeId(const N2npNodeId& nodeid)
+    :name(nodeid.name), relay(nodeid.relay) {
+    }
+
+    N2npNodeId& N2npNodeId::operator=(const N2npNodeId& nodeid) {
+        if (this != &nodeid) {
+            this->name = nodeid.name;
+            this->relay = nodeid.relay;
+        }
+        return *this;
+    }
+
+    std::ostream& operator<<(std::ostream& os, const N2npNodeId& nodeid) {
         return os << nodeid.name << '@' << nodeid.relay;
     }
 
-    std::ostream& operator<<(std::ostream& os, const N2npNodeId *nodeid)
-    {
-        if (!nodeid)
-            return os << "(null)";
-        else
-            return os << (*nodeid);
+    std::string N2npNodeId::toString() const {
+        std::ostringstream stream;
+        stream << *this;
+        return stream.str();
     }
 
-    bool operator==(const N2npNodeId& nodeId1, const N2npNodeId& nodeId2)
-    {
-        return (!strncmp(nodeId1.name, nodeId2.name, N2npNodeNameMaxLen))
-            && (nodeId1.relay == nodeId2.relay);
+    bool operator==(const N2npNodeId& nodeid1, const N2npNodeId& nodeid2) {
+        return (!nodeid1.name.compare(nodeid2.name))
+            && (nodeid1.relay == nodeid2.relay);
     }
 
-    std::string N2npNodeId::getName(void) const
-    {
-        std::string s(this->name);
-        return s;
+    bool operator!=(const N2npNodeId& nodeid1, const N2npNodeId& nodeid2) {
+        return !(nodeid1 == nodeid2);
+    }
+
+    const std::string& N2npNodeId::getName() const {
+        return name;
+    }
+
+    const Address& N2npNodeId::getRelay() const {
+        return relay;
     }
 }
