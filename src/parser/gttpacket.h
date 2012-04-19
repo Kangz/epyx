d8bd7d711c2c86434659810d91f3232daa666e10
Nicolas Iooss <nicolas.iooss@polytechnique.org>
2012-04-19 12:21:08 +0200

    Doc: document GTTPacket
    Signed-off-by: Nicolas Iooss <nicolas.iooss@polytechnique.org>

--- src/parser/gttpacket.h
+++ src/parser/gttpacket.h
@@ -20,17 +20,39 @@ namespace Epyx
     class GTTPacket
     {
     public:
+        /**
+         * @brief Constructor
+         */
         GTTPacket();
+
         ~GTTPacket();
+
+        /**
+         * @brief Print a short packet desciption in an output stream (for debug)
+         * @param os output stream
+         * @param pkt
+         */
         friend std::ostream& operator<<(std::ostream& os, const GTTPacket& pkt);
 
         /**
-         * Public variables
+         * @brief Protocol name
          */
         std::string protocol;
+        /**
+         * @brief Method name
+         */
         std::string method;
+        /**
+         * @brief Headers map
+         */
         std::map<std::string, std::string> headers;
+        /**
+         * @brief Content size
+         */
         long size;
+        /**
+         * @brief Content body
+         */
         char *body;
     };
 }
