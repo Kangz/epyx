640a10aa173fe23eee0894417edb1e9804096d82
Corentin Wallez <corentin@wallez.net>
2012-04-19 13:12:10 +0200

    DHT: Parse and output valid peer lists

--- src/dht/peer.cpp
+++ src/dht/peer.cpp
@@ -1,13 +1,28 @@
 #include "peer.h"
+#include <sstream>
 
 namespace Epyx
 {
 namespace DHT
 {
 
+    const char DHT_FOUND_PEERS_DELIM = '%';
+
     //TODO after we have a serialize/unserialize for N2npNodeId
-    void Peer::serialize(std::ostream& o){}
-    void Peer::unserialize(std::istream& i){}
+    void Peer::serialize(std::ostream& o){
+        o << n2npId;
+        o << DHT_FOUND_PEERS_DELIM;
+        o << id;
+    }
+
+    void Peer::unserialize(std::istream& i){
+        std::stringbuf sb;
+        i.get(sb, DHT_FOUND_PEERS_DELIM);
+        n2npId = N2NP::NodeId(sb.str());
+
+        i.ignore(); //remove the delimiter
+        i >> id;
+    }
 
 }
 }
