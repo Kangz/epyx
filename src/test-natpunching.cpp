75e3bc9443a10cd9159a6c3732e4e13a4e93f0fb
Nicolas Iooss <nicolas.iooss+git@polytechnique.org>
2012-11-11 19:25:24 +0100

    Clean up old things in test-natpunching
    Signed-off-by: Nicolas Iooss <nicolas.iooss+git@polytechnique.org>

--- src/test-natpunching.cpp
+++ src/test-natpunching.cpp
@@ -1,25 +1,5 @@
 #include "api.h"
 #include "natpunching/upnp/openconnect.h"
-/*
-#include <iostream>
-#include <sstream>
-#include <map>
-#include "core/common.h"
-#include "net/socket.h"
-#include "net/address.h"
-#include "natpunching/upnp/command.h"
-#include "natpunching/upnp/openconnect.h"
-#include "natpunching/upnp/igd.h"
- */
-/*
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <stdio.h>
-#include <arpa/inet.h>
-#define SOCKET_ERROR (-1)
- */
 #include "natpunching/upnp/discovery.h"
 #include "net/uri.h"
 
@@ -73,7 +53,7 @@ bool test_IGD() {
     return true;
 }
 
-int main(int argc, char* argv[]) {
+int main() {
     Epyx::API epyx;
     try {
         //Epyx::UPNP::Natpunch natpunch;
@@ -83,41 +63,4 @@ int main(int argc, char* argv[]) {
         Epyx::log::fatal << e << Epyx::log::endl;
     }
     return 0;
-
-    //std::string address, service, path;
-    //unsigned short port;
-
-    /*
-    std::cout << "Enter Address to connect with : " ;
-    std::cin >> address;
-    std::cout << "Enter Port : ";
-    std::cin >> port;
-    std::cout << "Enter Path to rootDesc.xml : " ;
-    std::cin >> path;
-
-    std::string blah;
-
-    std::cout << "The command is being created ..." << std::endl;
-    Epyx::UPNP::IGD igd;
-    igd.setAddress(address, port);
-    igd.setRootDescPath(path);
-    igd.getServices();
-    std::map<std::string,std::string> services = igd.getServiceList();
-    for (std::map<std::string,std::string>::iterator it=services.begin(); it != services.end(); ++it){
-        std::cout << it->first << " : " << it->second << std::endl;
-    }
-    std::cout << "IGD Configured. Adding port map" << std::endl;
-    try{
-        Epyx::Address addr = igd.addPortMap(22,Epyx::UPNP::TCP,1337);
-        std::cout << "Port map added" << std::endl;
-        std::cout << "Does it work? ";
-        std::cin >> blah;
-        std::cout << "Deleting Port Map" << std::endl;
-        igd.delPortMap(addr,Epyx::UPNP::TCP);
-        std::cout << "Port Map deleted" << std::endl;
-    }
-    catch(Epyx::FailException e){
-        std::cerr << e << std::endl;
-    }
-     */
 }
